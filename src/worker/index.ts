17bd9b62b68b735400d3b4fc76d7b5ad0f40f400
Daniel Szelag <daniel.szelag@SBPL4GN95G7.local>
2025-06-26 14:28:02 +0200

    change name

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,6 +1,6 @@
-import { Hono } from "hono";
-const app = new Hono<{ Bindings: Env }>();
+import { Hono } from 'hono'
+const app = new Hono<{ Bindings: Env }>()
 
-app.get("/api/", (c) => c.json({ name: "Cloudflare" }));
+app.get('/api/', (c) => c.json({ name: 'dani' }))
 
-export default app;
+export default app
